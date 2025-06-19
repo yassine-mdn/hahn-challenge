56d428e67c5e6d02fb83caba57c1caf402f24fc4
yassine <yassinemouddene@gmail.com>
2025-06-19 17:10:25 +0100

    feat:created new spring project

---
+++ bookspace-back/src/main/java/io/hahn/bookspaceback/BookspaceBackApplication.java
@@ -0,0 +1,13 @@
+package io.hahn.bookspaceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookspaceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookspaceBackApplication.class, args);
+    }
+
+}
