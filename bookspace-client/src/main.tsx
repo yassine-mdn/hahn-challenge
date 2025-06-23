06285689982daa5fb195afc1aef5243ff55b2cd0
yassine <yassinemouddene@gmail.com>
2025-06-23 16:55:58 +0100

    feat: added needed dependencies

--- bookspace-client/src/main.tsx
+++ bookspace-client/src/main.tsx
@@ -1,14 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from "react-router";
+import {BrowserRouter} from "react-router";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <App/>
+                <ReactQueryDevtools initialIsOpen={false} position={"right"}/>
+            </QueryClientProvider>
+        </BrowserRouter>
+    </StrictMode>,
 )
