41768addb1115b2a4e44017a68d43faf0804870b
yassine <yassinemouddene@gmail.com>
2025-06-24 00:54:09 +0100

    feat: added footer

--- bookspace-client/src/App.tsx
+++ bookspace-client/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import {Route, Routes} from "react-router";
 import BaseLayout from "@/layouts/BaseLayout.tsx";
+import Home from "@/routes/Home.tsx";
 
 function App() {
 
     return (
         <Routes>
             <Route element={<BaseLayout/>}>
-                <Route index element={<div />} />
+                <Route index element={<Home />} />
+                <Route path={"my-books"} element={<span>hhhhh</span>}/>
             </Route>
         </Routes>
     )
