df15c3cff716d4ae9ffaa41a96d4c4001ea65ca8
yassine <yassinemouddene@gmail.com>
2025-06-24 22:43:53 +0100

    fix: fixed type

--- bookspace-client/src/services/user.service.ts
+++ bookspace-client/src/services/user.service.ts
@@ -5,7 +5,7 @@ import type {UserDTO} from "@/types/user-dto.ts";
 
 export const updateUsername = async (curentName: string,newName:string): Promise<UserDTO> => {
   const response = await api.put(`/api/v1/users/${curentName}`, {
-    userName: newName,
+    username: newName,
   } );
   return response.data;
 }; 
