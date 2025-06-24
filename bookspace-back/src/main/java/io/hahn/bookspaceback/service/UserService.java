6e54ff9d19804c1ef46d240306c0e2f1c19d56b5
yassine <yassinemouddene@gmail.com>
2025-06-24 22:37:09 +0100

    fix: getByUsername now acctualy getby username and not id

--- bookspace-back/src/main/java/io/hahn/bookspaceback/service/UserService.java
+++ bookspace-back/src/main/java/io/hahn/bookspaceback/service/UserService.java
@@ -6,12 +6,14 @@ import io.hahn.bookspaceback.exception.CustomException;
 import io.hahn.bookspaceback.mapper.UserMapper;
 import io.hahn.bookspaceback.repository.UserRepository;
 import io.hahn.bookspaceback.util.PageWrapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @Service
@@ -65,6 +67,29 @@ public class UserService {
             log.error("Error updating user with id {} : {}", id, ex.getMessage(), ex);
             throw new CustomException("Failed to update user with id " + id + " : " + ex);
         }
+    }
+    
+    //added last minute intead of modifying existing one because of tests
+    public UserDTO updateByUsername(String username, @Valid @RequestBody UserDTO userDTO) {
+        try {
+            User updatedUser = userRepository.findByUsername(username)
+                    .map(user -> {
+                        userMapper.updateUserFromDto(userDTO, user);
+                        return user;
+                    })
+                    .orElseThrow(() -> {
+                        log.error("User with username {} not found", username);
+                        return new CustomException("User with username " + username + " not found", HttpStatus.NOT_FOUND);
+                    });
+
+            updatedUser = userRepository.save(updatedUser);
+            return userMapper.toDTO(updatedUser);
+        } catch (CustomException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            log.error("Error updating user with username {} : {}", username, ex.getMessage(), ex);
+            throw new CustomException("Failed to update user with username " + username + " : " + ex);
+        } 
     }
 
     public UserDTO getById(String id) {
