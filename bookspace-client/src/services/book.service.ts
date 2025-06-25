231c59f363ee8ffda89bb6e1ec39ca8ea3d22c83
yassine <yassinemouddene@gmail.com>
2025-06-25 02:29:16 +0100

    feat: fetch featured and popular books from api

--- bookspace-client/src/services/book.service.ts
+++ bookspace-client/src/services/book.service.ts
@@ -1,9 +1,9 @@
 // /api/book.service.ts
-import type { BookDTO } from "@/types/book-dto";
-import type { RatingCountDTO } from "@/types/rating-count-dto";
-import type { ReviewDTO } from "@/types/review-dto";
+import type {BookDTO} from "@/types/book-dto";
+import type {RatingCountDTO} from "@/types/rating-count-dto";
+import type {ReviewDTO} from "@/types/review-dto";
 import instance from "./axios";
-import type {PageWrapperBookDTO} from "@/types/page-wrapper-book-dto.ts";
+import type {PageWrapper} from "@/types/page-wrapper.ts";
 
 export const fetchBookDetails = async (id: number): Promise<BookDTO> =>
   (await instance.get(`api/v1/books/${id}`)).data;
@@ -17,7 +17,13 @@ export const fetchReviews = async (id: number): Promise<ReviewDTO[]> =>
 export const fetchRatings = async (id: number): Promise<RatingCountDTO[]> =>
   (await instance.get(`api/v1/books/${id}/ratings`)).data;
 
-export const fetchAllBooks = async (keywork:string, pageNumber: number, pageSize:number): Promise<PageWrapperBookDTO> =>
+export const fetchPopularBooks = async () : Promise<PageWrapper<BookDTO>> =>
+    (await instance.get(`api/v1/books/popular`)).data;
+
+export const fetchFeaturedBooks = async () : Promise<PageWrapper<BookDTO>> =>
+    (await instance.get(`api/v1/books/featured`)).data;
+
+export const fetchAllBooks = async (keywork:string, pageNumber: number, pageSize:number): Promise<PageWrapper<BookDTO>> =>
     (await instance.get(`api/v1/books`, {
         params: {
             search: keywork,
