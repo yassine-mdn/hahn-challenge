f09589c8881e82d074525527b7aa894a9f53a570
yassine <yassinemouddene@gmail.com>
2025-06-25 03:02:44 +0100

    feat: book details now fetches from database

--- bookspace-client/src/services/book.service.ts
+++ bookspace-client/src/services/book.service.ts
@@ -1,18 +1,13 @@
-// /api/book.service.ts
 import type {BookDTO} from "@/types/book-dto";
 import type {RatingCountDTO} from "@/types/rating-count-dto";
-import type {ReviewDTO} from "@/types/review-dto";
 import instance from "./axios";
 import type {PageWrapper} from "@/types/page-wrapper.ts";
 
 export const fetchBookDetails = async (id: number): Promise<BookDTO> =>
   (await instance.get(`api/v1/books/${id}`)).data;
 
-export const fetchSimilarBooks = async (id: number): Promise<BookDTO[]> =>
+export const fetchSimilarBooks = async (id: number): Promise<PageWrapper<BookDTO>> =>
   (await instance.get(`api/v1/books/${id}/similar`)).data;
-
-export const fetchReviews = async (id: number): Promise<ReviewDTO[]> =>
-  (await instance.get(`api/v1/reviews/${id}/reviews`)).data;
 
 export const fetchRatings = async (id: number): Promise<RatingCountDTO[]> =>
   (await instance.get(`api/v1/books/${id}/ratings`)).data;
